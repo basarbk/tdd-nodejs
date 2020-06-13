6a46faebd61e37cdbb35fe4302a4446814ab7594
Basar Buyukkahraman <basarbk@yahoo.com>
2020-06-13 23:49:55 +0300

    0203-hashing-password

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -73,4 +73,21 @@ describe('User Registration', () => {
         });
       });
   });
+
+  it('hashes the password in database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          const savedUser = userList[0];
+          expect(savedUser.password).not.toBe('P4ssword');
+          done();
+        });
+      });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const User = require('./user/User');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/api/1.0/users', (req, res) => {
-  User.create(req.body).then(() => {
-    return res.send({ message: 'User created' });
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = { ...req.body, password: hash };
+    User.create(user).then(() => {
+      return res.send({ message: 'User created' });
+    });
   });
 });
 
