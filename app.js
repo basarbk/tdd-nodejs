06d006082979dcfbb928cf3cace84966e1d6581d
Basar Buyukkahraman <basarbk@yahoo.com>
2020-06-13 22:19:23 +0300

    0102-dev-dependencies

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log("app is running"));
+app.listen(3000, () => console.log('app is running!'));
