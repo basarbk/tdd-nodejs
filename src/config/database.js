6a9ac294cc28e27d638325bfd5dc64b838584243
Basar Buyukkahraman <basarbk@yahoo.com>
2020-06-14 00:30:46 +0300

    0301-env-config

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
+const config = require('config');
 
-const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false,
+const dbConfig = config.get('database');
+
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  dialect: dbConfig.dialect,
+  storage: dbConfig.storage,
+  logging: dbConfig.logging,
 });
 
 module.exports = sequelize;
