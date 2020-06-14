8cd2372a428c41712a208dcf7aed936279a1a30d
Basar Buyukkahraman <basarbk@yahoo.com>
2020-06-14 10:00:43 +0300

    0402-validator-middleware

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -79,4 +79,22 @@ describe('User Registration', () => {
     const body = response.body;
     expect(body.validationErrors.username).toBe('Username cannot be null');
   });
+  it('returns E-mail cannot be null when email is null', async () => {
+    const response = await postUser({
+      username: 'user1',
+      email: null,
+      password: 'P4ssword',
+    });
+    const body = response.body;
+    expect(body.validationErrors.email).toBe('E-mail cannot be null');
+  });
+  it('returns errors for both when username and email is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: null,
+      password: 'P4ssword',
+    });
+    const body = response.body;
+    expect(Object.keys(body.validationErrors)).toEqual(['username', 'email']);
+  });
 });
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -2,16 +2,32 @@ const express = require('express');
 const UserService = require('./UserService');
 const router = express.Router();
 
-router.post('/api/1.0/users', async (req, res) => {
+const validateUsername = (req, res, next) => {
   const user = req.body;
   if (user.username === null) {
-    return res.status(400).send({
-      validationErrors: {
-        username: 'Username cannot be null',
-      },
-    });
+    req.validationErrors = {
+      username: 'Username cannot be null',
+    };
   }
+  next();
+};
 
+const validateEmail = (req, res, next) => {
+  const user = req.body;
+  if (user.email === null) {
+    req.validationErrors = {
+      ...req.validationErrors,
+      email: 'E-mail cannot be null',
+    };
+  }
+  next();
+};
+
+router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
+  if (req.validationErrors) {
+    const response = { validationErrors: { ...req.validationErrors } };
+    return res.status(400).send(response);
+  }
   await UserService.save(req.body);
   return res.send({ message: 'User created' });
 });
