e1192f5a84f3133ba3640c93cc6d7dc70281dd9c
Basar Buyukkahraman <basarbk@yahoo.com>
2020-08-04 18:39:42 +0300

    1406-organizing-npm-scripts

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
 const development = require('./development');
 const staging = require('./staging');
 const test = require('./test');
+const production = require('./production');
 
 module.exports = {
   development,
   staging,
   test,
+  production,
 };
---
+++ config/production.js
@@ -0,0 +1,20 @@
+module.exports = {
+  database: {
+    database: 'hoaxify',
+    username: 'my-db-user',
+    password: 'db-p4ss',
+    dialect: 'sqlite',
+    storage: './prod-db.sqlite',
+    logging: false,
+  },
+  mail: {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'hannah.stracke62@ethereal.email',
+      pass: 'Udhq8A43hSujuuSPS7',
+    },
+  },
+  uploadDir: 'uploads-production',
+  profileDir: 'profile',
+};
