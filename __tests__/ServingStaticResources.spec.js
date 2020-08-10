4188afeebaa309c13042df0aaa9aca4f237fef1c
Basar Buyukkahraman <basarbk@yahoo.com>
2020-08-10 23:44:31 +0300

    1704-serving-attachment-folder

--- __tests__/ServingStaticResources.spec.js
+++ __tests__/ServingStaticResources.spec.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const path = require('path');
 const config = require('config');
 
-const { uploadDir, profileDir } = config;
+const { uploadDir, profileDir, attachmentDir } = config;
 const profileFolder = path.join('.', uploadDir, profileDir);
+const attachmentFolder = path.join('.', uploadDir, attachmentDir);
 
 describe('Profile Images', () => {
   const copyFile = () => {
@@ -28,6 +29,32 @@ describe('Profile Images', () => {
   it('returns cache for 1 year in response', async () => {
     const storedFileName = copyFile();
     const response = await request(app).get('/images/' + storedFileName);
+    const oneYearInSeconds = 365 * 24 * 60 * 60;
+    expect(response.header['cache-control']).toContain(`max-age=${oneYearInSeconds}`);
+  });
+});
+
+describe('Attachments', () => {
+  const copyFile = () => {
+    const filePath = path.join('.', '__tests__', 'resources', 'test-png.png');
+    const storedFileName = 'test-attachment-file';
+    const targetPath = path.join(attachmentFolder, storedFileName);
+    fs.copyFileSync(filePath, targetPath);
+    return storedFileName;
+  };
+  it('returns 404 when file not found', async () => {
+    const response = await request(app).get('/attachments/123456');
+    expect(response.status).toBe(404);
+  });
+
+  it('returns 200 ok when file exist', async () => {
+    const storedFileName = copyFile();
+    const response = await request(app).get('/attachments/' + storedFileName);
+    expect(response.status).toBe(200);
+  });
+  it('returns cache for 1 year in response', async () => {
+    const storedFileName = copyFile();
+    const response = await request(app).get('/attachments/' + storedFileName);
     const oneYearInSeconds = 365 * 24 * 60 * 60;
     expect(response.header['cache-control']).toContain(`max-age=${oneYearInSeconds}`);
   });
--- src/app.js
+++ src/app.js
@@ -12,8 +12,9 @@ const path = require('path');
 const HoaxRouter = require('./hoax/HoaxRouter');
 const FileRouter = require('./file/FileRouter');
 
-const { uploadDir, profileDir } = config;
+const { uploadDir, profileDir, attachmentDir } = config;
 const profileFolder = path.join('.', uploadDir, profileDir);
+const attachmentFolder = path.join('.', uploadDir, attachmentDir);
 
 const ONE_YEAR_IN_MILLIS = 365 * 24 * 60 * 60 * 1000;
 
@@ -42,6 +43,8 @@ app.use(middleware.handle(i18next));
 app.use(express.json({ limit: '3mb' }));
 
 app.use('/images', express.static(profileFolder, { maxAge: ONE_YEAR_IN_MILLIS }));
+
+app.use('/attachments', express.static(attachmentFolder, { maxAge: ONE_YEAR_IN_MILLIS }));
 
 app.use(tokenAuthentication);
 
